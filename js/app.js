2437c344f6c4ea73ce5a84d3657b0392ec9440bc
Alvian Joseph <alvianjoseph@gmail.com>
2019-04-04 18:35:28 -0700

    added a loop and function to question 6 and 7

--- js/app.js
+++ js/app.js
@@ -74,27 +74,54 @@ if(doILoveGoKarts === "y" || doILoveGoKarts === "yes") {
 }
 
 
-//what we worked on
+
 var guessesRemaining = 5; //Guess Counter
-var numberOfGames = Number(prompt("Guess how many games Ive been featured in!" + "\n" + " Hint: It\'s an amount of spotted dogs."));
-//inital prompt, first number is input
+var numberOfGames = 1; // AJ - placeholder variable so our while loop recognizes it
+                       //the value is changed with user input
+
 
 
 while(guessesRemaining > 0 && numberOfGames !== 101){
+    numberOfGames = Number(prompt("Guess how many games Ive been featured in!" + "\n" + " Hint: It\'s an amount of spotted dogs."));
     guessesRemaining--;
-    }if(numberOfGames === 101){
-    alert("YA, YA, YA, YA, YA!!");
-    }else if(guessesRemaining > 0 && numberOfGames!== 101){
-    numberOfGames = Number(prompt('Oh No! You Only have ' + guessesRemaining + ' untill you lose!'));
-    }else{
-    alert("Ba Ba Baa Da Da Daa. GAME OVER");
+    if(numberOfGames > 101 && guessesRemaining >= 1){
+        alert("Too many. Chances remaining: " + guessesRemaining)// AJ You can adjust to your liking
+    }else if(numberOfGames < 101 && guessesRemaining >= 1){
+        alert('Too little. Chances remaining: ' + guessesRemaining)// AJ- adjust as needed
+    }
+    } 
+    if(numberOfGames === 101){
+        alert('Correct!');//AJ change to your liking
+    }else if(guessesRemaining ===0){
+        alert('you suck');//AJ - adjust to your liking
     }
 
+ guessesRemaining = 6;
+ var possibleAnswers = ['pizza', 'hamburgers', 'sushi', 'chicken', 'tacos', 'mexican', 'chinese']; //AJ- you can change these values later, just getting you started
+ var userChoseCorrectly = false;
 
+function question7(){
+while(guessesRemaining > 0 && !userChoseCorrectly){
+    var joshFavoriteFood = prompt("What's Josh's favorite food? There are lots of right answers")
+    guessesRemaining--;
+    console.log(joshFavoriteFood)
+    for(var i = 0; i < possibleAnswers.length; i++){
+        if(joshFavoriteFood === possibleAnswers[i]){
+            alert("That's right! I love " + joshFavoriteFood + "!")
+            userChoseCorrectly = true;}}
 
+        if(userChoseCorrectly === false && guessesRemaining > 1){
+            alert('Nope, try again!')} 
+        console.log('user guessed wrong');
+        
+        if(guessesRemaining === 1){
+        alert('last try');
+        continue;}
+    }
+if(userChoseCorrectly === false){
+        alert('better luck next time!')}
+}
+question7(); // AJ - This calls your function for the last question
+//I put this in as an example but you can implement a function however you want
 
-
-
-
-
-
+//AJ I deleted the extra spaces below the final code.
